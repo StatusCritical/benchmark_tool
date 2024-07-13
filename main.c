84e20bc69b4204d5a24ba69fc201644f23f5e6a6
Emmett Armstrong <yourlocaldndnerd@outlook.com>
2024-07-13 12:25:45 -0500

    runs multiple times in one file run.

--- main.c
+++ main.c
@@ -17,31 +17,6 @@ double endc(){
     return time_taken;
 }
 
-void gen_mat(int not){
-    int i = 300, j = 300;
-    int x = i, y = j;
-    float a[i][j];
-    float b[i][j];
-    float c[i][j];
-
-    startc();
-    int z;
-    for (z=0;z<not;z++) {
-        for (x=0;x<i;x++){
-            for (y=0;y<j;y++){
-                a[x][y] = 2;
-            }
-        }
-        for (x=0;x<i;x++){
-            for (y=0;y<j;y++){
-                a[x][y] = 0;
-            }
-        }
-    }
-    endc();
-    printf("%f", time_taken);
-}
-
 void mult_mat(int not){
     int i = 300, j = 300;
     int x = i, y = j;
@@ -79,13 +54,15 @@ void mult_mat(int not){
 }
 
 int main(void) {
-    int o;
-    float timetime;
-    for (o=0;o<10;o++) {
-        printf("Pass %d\n", o+1);
-        mult_mat(10000);
-        timetime += time_taken;
+    while (1) {
+        int o;
+        float timetime;
+        for (o = 0; o < 10; o++) {
+            printf("Pass %d\n", o + 1);
+            mult_mat(10000);
+            timetime += time_taken;
+        }
+        printf("%f\n", timetime / 10);
+        system("pause");
     }
-    printf("%f\n", timetime/10);
-    system("pause");
 }
