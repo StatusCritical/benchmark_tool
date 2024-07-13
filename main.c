2b807c889d2bfedb72f93ac40f750afe0575857d
Emmett Armstrong <yourlocaldndnerd@outlook.com>
2024-07-13 12:52:17 -0500

    Moved run logic to function.

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 clock_t t;
 double time_taken;
 float a = 10.0;
+int o;
+float timetime;
 
 void startc(){
     t = clock();
@@ -33,36 +35,41 @@ void mult_mat(int not){
             b[x][y] = 3;
         }
     }
+    for (o = 0; o < 10; o++) {
+        printf("Pass %d\n", o + 1);
 
-    startc();
-    int z;
-    for (z=0;z<not;z++) {
-        for (x = 0; x < i; x++) {
-            for (y = 0; y < j; y++) {
-                //c[x][y] = (a[x][y] * b[x][y]);
-                c[x][y] = ((float)rand() / RAND_MAX) * 500;
+        startc();
+        int z;
+        for (z = 0; z < not; z++) {
+            for (x = 0; x < i; x++) {
+                for (y = 0; y < j; y++) {
+                    //c[x][y] = (a[x][y] * b[x][y]);
+                    c[x][y] = ((float) rand() / RAND_MAX) * 500;
+                }
+            }
+            for (x = 0; x < i; x++) {
+                for (y = 0; y < j; y++) {
+                    c[x][y] = 0;
+                }
             }
         }
-        for (x = 0; x < i; x++) {
-            for (y = 0; y < j; y++) {
-                c[x][y] = 0;
-            }
-        }
+        endc();
+        timetime += time_taken;
     }
-    endc();
+    printf("%f\n", timetime / 10);
     //printf("%f", time_taken);
 }
 
 int main(void) {
     while (1) {
-        int o;
-        float timetime;
-        for (o = 0; o < 10; o++) {
-            printf("Pass %d\n", o + 1);
+//        int o;
+//        float timetime;
+//        for (o = 0; o < 10; o++) {
+//            printf("Pass %d\n", o + 1);
             mult_mat(10000);
-            timetime += time_taken;
-        }
-        printf("%f\n", timetime / 10);
+//            timetime += time_taken;
+//        }
+//        printf("%f\n", timetime / 10);
         system("pause");
         timetime = 0;
     }
